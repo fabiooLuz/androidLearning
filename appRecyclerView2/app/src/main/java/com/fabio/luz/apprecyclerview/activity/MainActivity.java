3896ff2c32ad962dd9f6499b6a6ff923690be4a2
fabiooluz <fabioanderson_sb@hotmail.com>
2021-05-31 22:44:19 -0300

    teste correção

---
+++ appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fabio.luz.apprecyclerview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fabio.luz.apprecyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/adapter/Adapter.java
+++ appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/adapter/Adapter.java
@@ -1,2 +1,34 @@
-package com.fabio.luz.apprecyclerview.adapter;public class Adapter {
+package com.fabio.luz.apprecyclerview.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter <Adapter.MyViewHolder>{
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+            super(itemView);
+        }
+    }
 }
