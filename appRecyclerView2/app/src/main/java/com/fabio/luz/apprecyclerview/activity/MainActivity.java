10b2b0604546f1474d98b2769a9ac5793ccc0723
fabiooluz <fabioanderson_sb@hotmail.com>
2021-06-03 23:52:30 -0300

    continuação do apprecylerView

--- appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/activity/MainActivity.java
+++ appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/activity/MainActivity.java
@@ -1,16 +1,85 @@
 package com.fabio.luz.apprecyclerview.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.fabio.luz.apprecyclerview.R;
+import com.fabio.luz.apprecyclerview.adapter.Adapter;
+import com.fabio.luz.apprecyclerview.model.Disciplina;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvRecyclerView;
+    private List<Disciplina> disciplinas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvRecyclerView = findViewById(R.id.rvRecyclerView);
+        //Configurar o adapter
+
+        Adapter adapter = new Adapter( disciplinas );
+
+        //Configurar o recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        rvRecyclerView.setLayoutManager( layoutManager);
+        rvRecyclerView.setHasFixedSize( true );
+
+        rvRecyclerView.setAdapter( adapter );
+
     }
+
+    public void criarDisciplinas() {
+
+        Disciplina d = new Disciplina("Lógica de Programação","Juan Baptista","SEG","LAB 05" );
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Estatística Computacional","Wellington Avila","SEG","SALA 107" );
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Teoria de Sistemas da Informação","Wellington Avila","TER","SALA 109" );
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Banco de Dados I","Fabrício Curvello","QUA","LAB 05");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Arquitetura de Computadores","Wellington Avila","QUA","LAB 05");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Programação Orientada a Objetos","Fabrício Curvello","QUA","LAB 04");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Computação para Dispositivos Móveis","Fabrício Curvello","QUI","LAB 02");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Estrutura de Dados","Juan Baptista","QUI","LAB 04");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Interface Humano-Computador","Wellington Avila","SEX","SALA 109");
+
+        disciplinas.add(d);
+
+        d = new Disciplina("Desevolvimento de Software para Web","Juan Baptista","SEX","LAB 02");
+
+        disciplinas.add(d);
+
+    }
+
+
 }
--- appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/adapter/Adapter.java
+++ appRecyclerView2/app/src/main/java/com/fabio/luz/apprecyclerview/adapter/Adapter.java
@@ -1,34 +1,67 @@
 package com.fabio.luz.apprecyclerview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fabio.luz.apprecyclerview.R;
+import com.fabio.luz.apprecyclerview.model.Disciplina;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter <Adapter.MyViewHolder>{
+
+    private List<Disciplina> listaDisciplina;
+
+    public Adapter( List<Disciplina> lista){
+        this.listaDisciplina = lista;
+    }
 
     @NonNull
     @org.jetbrains.annotations.NotNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
-        return null;
+        // método para criar a visualização dos itens da lista
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_lista, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.MyViewHolder holder, int position) {
+        // Método para exibir os itens da lista
+        Disciplina d = listaDisciplina.get( position);
+        holder.tvDisciplina.setText( d.getNomeDisciplina() );
+        holder.tvProfessor.setText(d.getProfessor());
+        holder.tvDiasemana.setText(d.getDiaSemana());
+        holder.tvSala.setText(d.getSala());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        //Quantidade de itens a ser exibidos
+
+        return 15;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView tvDisciplina;
+        TextView tvProfessor;
+        TextView tvDiasemana;
+        TextView tvSala;
 
         public MyViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
             super(itemView);
+
+            tvDisciplina = itemView.findViewById(R.id.tvDisciplina);
+            tvProfessor = itemView.findViewById(R.id.tvProfessor);
+            tvDiasemana = itemView.findViewById(R.id.tvDiasemana);
+            tvSala = itemView.findViewById(R.id.tvSala);
         }
     }
 }
